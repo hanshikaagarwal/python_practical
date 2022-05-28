37f2171fd36f96af6b66201ae441ba4f6db8cfff
hanshikaagarwal <106018147+hanshikaagarwal@users.noreply.github.com>
2022-05-28 23:44:19 +0530

    Add files via upload

---
+++ dictionary/dic2.py
@@ -0,0 +1,6 @@
+#ques2-create a dictionary called students containing names and ages .copy the dictionary into stud. empty the students dictionary 
+# as stud continues to hold the data
+students={'anil':23,'sanjay':28,'ajay':25}
+stud=students
+students={}
+print(stud)
