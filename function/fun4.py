967f293216988ad1c0f1f3847ffc789cb6280ccb
hanshikaagarwal <106018147+hanshikaagarwal@users.noreply.github.com>
2022-05-28 23:54:09 +0530

    Add files via upload

---
+++ function/fun4.py
@@ -0,0 +1,14 @@
+'''ques4-Write a program to add the sum of digits from 1 to 25,50 to 76,90 to 101 using three different for loops '''
+
+sum=0
+for i in range(1,26):
+    sum=sum+1
+print('sum of integers from 1 t0 25 is:',sum)
+sum=0
+for i in range(50,76):
+    sum=sum+1
+print('sum of integers from 50 to 76 is:',sum)
+sum=0
+for i in range(90,101):
+    sum=sum+1
+print('sum of integer from 90 to 100:',sum)
